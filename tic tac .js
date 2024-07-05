4eec6c5af1984b40fa66e975b871a024679dfd9a
Muskan1Kumari4draw <khushikumari00999.k@gmail.com>
2024-07-05 12:17:34 +0530

    hi

---
+++ tic tac .js
@@ -0,0 +1,75 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#resetbtn");
+let newGameBtn = document.querySelector("#New-btn");
+let msgcontainer = document.querySelector(".msg-container");
+let message = document.querySelector(".message");
+
+let turno = true;
+const winPattern = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+const resetGame = () => {
+  turno = true;
+  enableBoxes();
+  msgcontainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turno) {
+      box.innerText = "O";
+      turno = false;
+    } else {
+      box.innerText = "X";
+      turno = true;
+    }
+    box.disabled = true;
+
+    checkwinner();
+    let isWinner = checkwinner();
+    
+    if (count === 9 && !isWinner) {
+      gameDraw();
+    }
+  });
+});
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  message.innerText = `Congratulation, winner is ${winner}`;
+  msgcontainer.classList.remove("hide");
+  disableBoxes();
+};
+const checkwinner = () => {
+  for (pattern of winPattern) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos1Val === pos3Val) {
+        showWinner(pos1Val);
+      }
+    }
+  }
+};
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
